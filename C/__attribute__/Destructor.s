c4d43aef93f70e60df1101ae6d0fefc4cc8c6995
Wassim Bolles <106280014+wmBolles@users.noreply.github.com>
2024-03-30 01:06:29 +0000

    Update Destructor.s

--- C/__attribute__/Destructor.s
+++ C/__attribute__/Destructor.s
@@ -1,73 +1,51 @@
-	.section	__TEXT,__text,regular,pure_instructions
-	.build_version macos, 10, 15	sdk_version 10, 15, 6
-	.globl	__End                   ## -- Begin function _End
-	.p2align	4, 0x90
-__End:                                  ## @_End
-	.cfi_startproc
-## %bb.0:
+	.file	"_func_attributes.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"Printing in the init function"
+	.text
+	.globl	Begin
+	.type	Begin, @function
+Begin:
+	endbr64
 	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset %rbp, -16
 	movq	%rsp, %rbp
-	.cfi_def_cfa_register %rbp
-	leaq	L_.str(%rip), %rdi
-	movb	$0, %al
-	callq	_printf
-	popq	%rbp
-	retq
-	.cfi_endproc
-                                        ## -- End function
-	.globl	_main                   ## -- Begin function main
-	.p2align	4, 0x90
-_main:                                  ## @main
-	.cfi_startproc
-## %bb.0:
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset %rbp, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register %rbp
-	subq	$16, %rsp
-	movl	$0, -4(%rbp)
-	leaq	L_.str.1(%rip), %rdi
-	movb	$0, %al
-	callq	_printf
-	xorl	%ecx, %ecx
-	movl	%eax, -8(%rbp)          ## 4-byte Spill
-	movl	%ecx, %eax
-	addq	$16, %rsp
-	popq	%rbp
-	retq
-	.cfi_endproc
-                                        ## -- End function
-	.section	__TEXT,__StaticInit,regular,pure_instructions
-	.p2align	4, 0x90         ## -- Begin function __GLOBAL_init_65535
-___GLOBAL_init_65535:                   ## @__GLOBAL_init_65535
-	.cfi_startproc
-## %bb.0:
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset %rbp, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register %rbp
-	leaq	__End(%rip), %rax
-	xorl	%ecx, %ecx
-	movl	%ecx, %esi
+	leaq	.LC0(%rip), %rax
 	movq	%rax, %rdi
-	leaq	___dso_handle(%rip), %rdx
-	callq	___cxa_atexit
+	call	puts@PLT
+	nop
 	popq	%rbp
-	retq
-	.cfi_endproc
-                                        ## -- End function
-	.section	__TEXT,__cstring,cstring_literals
-L_.str:                                 ## @.str
-	.asciz	"printing in the destructor or the end function"
-
-L_.str.1:                               ## @.str.1
-	.asciz	"main\n"
-
-	.section	__DATA,__mod_init_func,mod_init_funcs
-	.p2align	3
-	.quad	___GLOBAL_init_65535
-.subsections_via_symbols
+	ret
+	.size	Begin, .-Begin
+	.section	.init_array,"aw"
+	.align 8
+	.quad	Begin
+	.text
+	.globl	main
+	.type	main, @function
+main:
+	endbr64
+	pushq	%rbp
+	movq	%rsp, %rbp
+	movl	$0, %eax
+	popq	%rbp
+	ret
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 11.4.0-1ubuntu1~22.04) 11.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:
